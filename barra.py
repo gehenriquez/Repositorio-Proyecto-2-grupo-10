10f3e0d0ca9a066ada1f345fd8ebc0384dc88ff1
TomasSchilling <69157271+TomasSchilling@users.noreply.github.com>
2020-09-28 20:47:54 -0300

    Update barra.py

--- barra.py
+++ barra.py
@@ -5,30 +5,31 @@ g = 9.81 #kg*m/s^2
 
 class Barra(object):
 
-	"""Constructor para una barra.
+    """Constructor para una barra.
     Los datos en orden son: nodo_i, nodo_j, radio, espesor, Modulo E, densidad, fluencia"""
-	def __init__(self, ni, nj, R, t, E, ρ, σy):
-		super(Barra, self).__init__()
-		self.ni = ni
-		self.nj = nj
-		self.R = R
-		self.t = t
-		self.E = E
-		self.ρ = ρ
-		self.σy = σy
+    def __init__(self, ni, nj, R, t, E, ρ, σy):
+        super(Barra, self).__init__()
+        self.ni = ni
+        self.nj = nj
+        self.R = R
+        self.t = t
+        self.E = E
+        self.ρ = ρ
+        self.σy = σy
 
-	def obtener_conectividad(self):
+    def obtener_conectividad(self):
+        return [self.n_i,self.n_j]
 
-		return None
+    def calcular_area(self):
+        """Calcula el area de una barra circular"""
+        return np.pi*(self.R**2-(self.R-self.t)**2)
 
-	def calcular_area(self):
-		"""Calcula el area de una barra circular"""
-		return np.pi*(self.R**2-(self.R-self.t)**2)
+    def calcular_largo(self, barra):
+        i=barra.obtener_coordenada_nodal(self.ni)
+        j=barra.obtener_coordenada_nodal(self.nj)
+        z=i-j
+        return np.sqrt(z[0]**2+z[1]**2)
 
-	def calcular_largo(self, reticulado):
-
-		return np.sqrt((self.ni[0]-self.nj[0])**2+(self.ni[1]-self.nj[1])**2)
-
-	def calcular_peso(self, barra):
+    def calcular_peso(self, barra):
             """Entrega el peso de una barra """
             self.ρ*calcular_largo(barra)*self.calcular_area()*g
